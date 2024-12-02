62d1f62a6b62bbabbb7a2421a0a6a962008a526e
Patricia <patricia.badji89@gmail.com>
2024-12-02 16:48:51 +0100

    bleh

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,23 +1,23 @@
 import { Component } from "react";
+import { Col, Row, Form } from "react-bootstrap";
 import SingleBook from "./SingleBook";
-import { Col, Form, Row } from "react-bootstrap";
 import CommentArea from "./CommentArea";
 
 class BookList extends Component {
   state = {
     searchQuery: "",
-    selectedBook: null,
+    selectedAsin: null,  // Stato per il codice asin del libro selezionato
   };
 
-  //come seleziono un libro? devo cambiare lo STATE. Fuori dal render sennò impazzisce
-
-  selectABook = (book) => {
-    this.setState({ selectedBook: book });
+  // Funzione per selezionare un libro
+  selectBook = (asin) => {
+    this.setState({ selectedAsin: asin });
   };
 
   render() {
     return (
       <>
+        {/* Ricerca libro */}
         <Row className="justify-content-center mt-5">
           <Col xs={12} md={4} className="text-center">
             <Form.Group>
@@ -32,32 +32,30 @@ class BookList extends Component {
         </Row>
 
         <Row className="g-2 mt-3">
+          {/* Colonna a sinistra: Griglia di libri */}
           <Col xs={6} md={4}>
             <Row>
-              {
-                //qui devo fare due colonne: una per i libri e una per la comment area
-                this.props.books
-                  .filter((b) =>
-                    b.title.toLowerCase().includes(this.state.searchQuery)
-                  )
-                  .map((b) => (
-                    <Col xs={6} md={4} key={b.asin}>
-                      <SingleBook
-                        book={b}
-                        //qui l'onClick
-                        onClick={() => this.selectABook(b)}
-                      />
-                    </Col>
-                  ))
-              }
+              {this.props.books
+                .filter((book) =>
+                  book.title.toLowerCase().includes(this.state.searchQuery.toLowerCase())
+                )
+                .map((book) => (
+                  <Col xs={6} md={4} key={book.asin}>
+                    <SingleBook
+                      book={book}
+                      onClick={() => this.selectBook(book.asin)}  // Passiamo il codice asin al click
+                    />
+                  </Col>
+                ))}
             </Row>
           </Col>
+
+          {/* Colonna a destra: CommentArea */}
           <Col xs={6} md={4}>
-            {/*operatore ternario per mostrare o meno la comment area*/}
-            {this.state.selectedBook ? (
-              <CommentArea book={this.state.selectedBook} />
+            {this.state.selectedAsin ? (
+              <CommentArea asin={this.state.selectedAsin} />
             ) : (
-              <div></div>
+              <div>Seleziona un libro per vedere i commenti!</div>
             )}
           </Col>
         </Row>
--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,68 +1,65 @@
 import { Component } from "react";
-
 import { Card } from "react-bootstrap";
 
-
-
-
 class CommentArea extends Component {
-	state = { CommentArea: [] };
+  state = {
+    comments: [],  // Stato per i commenti
+  };
 
-	getComments = () => {
-		fetch("https://striveschool-api.herokuapp.com/api/comments/0786966246", {
-			headers: {
-				Authorization:
-					"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTQzZDQzNmI0MDZiZTAwMTRiN2I3NmMiLCJpYXQiOjE3MzI4MDM2ODksImV4cCI6MTczNDAxMzI4OX0.X4pcqft6TnYz8rwNwlObvbskmjwu9S9gsW0N32HWMQY",
-				"Content-Type": "application/json",
-			},
-		})
-			.then((response) => {
-				if (response.ok) {
-					return response.json();
-				} else {
-					throw new Error();
-				}
-			})
 
-			.then((arrayOfComments) => {
-				this.setState({
-					CommentArea: arrayOfComments,
-				});
-			})
-			.catch((err) => {
-				console.log("errore", err);
-			});
-	};
+  getComments = () => {
+    const { asin } = this.props; 
 
-	componentDidUpdate(prevProps){
-		if(this.props.comment !== prevProps.comment) 
-		{
-			this.getComments()
-		}
+    fetch(`https://striveschool-api.herokuapp.com/api/comments/${asin}`, {
+      headers: {
+       "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTQzZDQzNmI0MDZiZTAwMTRiN2I3NmMiLCJpYXQiOjE3MzMxNTQwMTUsImV4cCI6MTczNDM2MzYxNX0.OIn3Z7VUA4HhPpkLH_yZ0BWcClcx5Lj_gKNJoWQRo44", 
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Errore nel recupero dei commenti");
+        }
+      })
+      .then((comments) => {
+        this.setState({ comments });
+      })
+      .catch((error) => {
+        console.error("Errore:", error);
+      });
+  };
 
-	}
 
-	componentDidMount() {
-		this.getComments();
-	};
+  componentDidMount() {
+    this.getComments();
+  }
 
-	render() {
-		return (
-			<>
-				{this.state.CommentArea.map((comm) => {
-					return (
-						<Card.Text>
-								{this.state.comm.comment}
-								<br />
-								{this.state.comm.rate}!
-                                </Card.Text>
-					);
-				})}
-			</>
-		);
-	}
+  
+  componentDidUpdate(prevProps) {
+    if (this.props.asin !== prevProps.asin) {
+      this.getComments();
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.comments.length === 0 ? (
+          <div>Nessun commento trovato per questo libro.</div>
+        ) : (
+          this.state.comments.map((comment) => (
+            <Card.Text key={comment._id}>
+              <strong>{comment.comment}</strong>
+              <br />
+              Rating: {comment.rate}!
+            </Card.Text>
+          ))
+        )}
+      </div>
+    );
+  }
 }
-
-
 
 export default CommentArea;
